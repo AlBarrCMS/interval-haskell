f8eca27901d6d4278e8eb09d8543965165747f0a
Mark Gillespie <mgillesp@caltech.edu>
2016-10-07 13:25:46 -0700

    Set up cabal build system

--- src/TestNaive.hs
+++ src/TestNaive.hs
@@ -1,10 +1,3 @@
-{-|
-Module : Main
--}
-module Main (
-    main
-) where
-
 import ImageWriter
 import Interval
 import IntervalNewton
@@ -17,7 +10,7 @@ import Data.Maybe
 import System.Environment
 
 -- | Main method. Currently prints out the result of Interval Newton run on the
--- command line arguments to a ppm or csv file. The first argument is the polynomial as 
+-- command line arguments to a ppm or csv file. The first argument is the polynomial as
 -- a string and the second is the output file name. The file name must end in either
 -- .ppm or .csv
 main :: IO ()
@@ -25,15 +18,18 @@ main = do
     args <- getArgs
     p_string <- return $ args !! 0
     filename <- return $ args !! 1
-    poly <- return $ fromJust $ parse_polynomial p_string  
+    poly <- return $ fromJust $ parse_polynomial p_string
     putStrLn $ "Polynomial parsed as: " ++ (show poly)
 
     p_zeroes <- return $! zeros poly "xy" [Interval (-3) 3, Interval (-3) 3] 0.001
     image <- return $ write_to_image p_zeroes 6.0 6.0 3.0 3.0 500 500
     tree_string <- return $! leaves poly "xy" [Interval (-3) 3, Interval (-3) 3] 0.001
-    if isSuffixOf ".csv" filename then
+    if isSuffixOf ".csv" filename
+      then
         writeFile filename $ write_leaf_data tree_string
-    else if isSuffixOf ".ppm" filename then
-        writeFile filename (write_to_ppm 255 image)
-    else 
-        putStrLn "Error: invalid filename"
+      else
+        if isSuffixOf ".ppm" filename
+          then
+            writeFile filename (write_to_ppm 255 image)
+          else
+              putStrLn "Error: invalid filename"
