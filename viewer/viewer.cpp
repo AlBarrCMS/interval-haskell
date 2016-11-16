9c49848eae41916efd7ae0f55111c7b9867cdbc5
Mark Gillespie <mgillesp@caltech.edu>
2016-11-15 16:30:50 -0800

    Updated Viewer
    I changed the program to outline your selection while zooming.
    I added some noise to the colors so that the boxes are easier to tell
    apart.

--- viewer/viewer.cpp
+++ viewer/viewer.cpp
@@ -1,20 +1,5 @@
 #include "viewer.h"
 
-#include <GL/glut.h>
-
-#include <cmath>
-#include <fstream>
-#include <limits>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-
-#include "json/json.h"
-
-#define XSCALE ((xmax - xmin) / 2)
-#define YSCALE ((ymax - ymin) / 2)
-
 double xmin;
 double xmax;
 double ymin;
@@ -22,27 +7,36 @@ double ymax;
 double current_min;
 Mode display_mode;
 
+int width  = 800;
+int height = 800;
 
 int xc;
 int yc;
-bool dirty;
 FILE *rin;
 std::string cmd;
+std::string polynomial;
 std::vector<Box> potential_solutions;
 std::vector<Box> discarded_boxes;
+Box *mouse_rect;
 
 
 void displayFunc() {
   glClear(GL_COLOR_BUFFER_BIT);
 
-  for (auto box = discarded_boxes.begin(); box != discarded_boxes.end(); ++box) {
-    draw_box(*box);
+  for (auto box = discarded_boxes.begin();
+            box != discarded_boxes.end(); ++box) {
+    draw_box(*box, false);
     draw_outline(*box, false);
   }
 
-  for (auto box = potential_solutions.begin(); box != potential_solutions.end(); ++box) {
-    draw_box(*box);
+  for (auto box = potential_solutions.begin();
+            box != potential_solutions.end(); ++box) {
+    draw_box(*box, true);
     draw_outline(*box, true);
+  }
+
+  if (mouse_rect) {
+    draw_outline(*mouse_rect, true);
   }
 
   glutSwapBuffers();
@@ -52,74 +46,73 @@ void idleFunc(){
   char line[1024];
   Box box;
 
-  if (dirty) {
-    potential_solutions.clear();
-    discarded_boxes.clear();
-    dirty = false;
-  }
+  // Read in up to 10 boxes before the next frame
+  int num_boxes = 50;
 
-  if (fgets(line, 1024, rin)) {
-    Json::Value val;
-    Json::Reader reader;
+  // Try to read in input
+  for (int i = 0; i < num_boxes; i++) {
+    if (fgets(line, 1024, rin)) {
+      Json::Value val;
+      Json::Reader reader;
 
-    if (reader.parse(line, val)) {
-      if (val.isObject()) {
-        double low = val["output"]["low"].asDouble();
-        double high = val["output"]["high"].asDouble();
-        if (high < current_min) {
-          current_min = high;
-        }
+      if (reader.parse(line, val)) {
+        if (val.isObject()) {
 
-        box.x0 = val["input"][0]["low"].asDouble() / XSCALE;
-        box.x1 = val["input"][0]["high"].asDouble() / XSCALE;
-        box.y0 = val["input"][1]["low"].asDouble() / YSCALE;
-        box.y1 = val["input"][1]["high"].asDouble() / YSCALE;
-        box.high = high;
-        box.low = low;
-        switch (display_mode) {
-          case roots:
-            box.valid = high > 0 && low < 0;
-            break;
-          case minimization:
-            box.valid = low < current_min;
-            prune_min_solution_boxes(current_min);
-            break;
-        }
+          // If we are computing a minimum, we need to keep track of the
+          // minimum
+          if (display_mode == minimization) {
+            if (val.isMember("min")) {
+              current_min = val["min"].asDouble();
+              prune_min_solution_boxes(current_min);
+            }
 
-        low = (high + low) / 2;
-        high = std::min(log(abs(low) + 1), 1.0) * 0.6 + 0.4;
-        box.r = low < 0 ? high : 0.2;
-        box.g = 0.2;
-        box.b = low > 0 ? high : 0.2;
-        box.next = NULL;
+            double high = val["output"]["high"].asDouble();
+            if (high < current_min) {
+              current_min = high;
+            }
+          }
 
-        if (box.valid) {
-          potential_solutions.push_back(box);
-        } else {
-          discarded_boxes.push_back(box);
+          box = Box(val, display_mode);
+
+          if (box.valid) {
+            potential_solutions.push_back(box);
+          } else {
+            discarded_boxes.push_back(box);
+          }
         }
       }
+      glutPostRedisplay();
     }
-    glutPostRedisplay();
   }
 }
 
 void init(int argc, char **argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE);
-  glutInitWindowSize(600, 600);
+  glutInitWindowSize(800, 800);
   glutCreateWindow("Hello world :D");
   glutDisplayFunc(displayFunc);
+  glutReshapeFunc(reshapeFunc);
   glutIdleFunc(idleFunc);
   glutMotionFunc(motionFunc);
   glutMouseFunc(mouseFunc);
   glutMainLoop();
 }
 
-void load() {
+FILE *load(std::string cmd, std::string polynomial, double xmin, double xmax,
+                                                    double ymin, double ymax) {
   std::stringstream s;
-  s << cmd << ' ' << xmin << ' ' << xmax << ' ' << ymin << ' ' << ymax;
-  rin = popen(s.str().c_str(), "r");
+  switch(display_mode) {
+    case roots:
+      s << cmd << " \"" << polynomial << "\" " << xmin << ' ' << xmax << ' '
+        << ymin << ' ' << ymax << " "<< (double) pow(XSCALE/1000, 2);
+      break;
+    case minimization:
+      s << cmd << " \"" << polynomial << "\" " << xmin << ' ' << xmax << ' '
+        << ymin << ' ' << ymax << " "<< (double) pow(XSCALE/200, 1);
+      break;
+  }
+  return popen(s.str().c_str(), "r");
 }
 
 int main(int argc, char **argv) {
@@ -139,6 +132,7 @@ int main(int argc, char **argv) {
   ymax = atof(argv[5]);
   display_mode = roots;
 
+
   if (argc >= 7) {
     if (atoi(argv[6]) == 1) {
       display_mode = minimization;
@@ -147,26 +141,28 @@ int main(int argc, char **argv) {
   }
 
   f.open(argv[1]);
-  std::getline(f, cmd);
+  std::getline(f, polynomial);
 
   switch (display_mode) {
     case minimization:
-      cmd = "dist/build/moore_skelboe/moore_skelboe \"" + cmd + "\"";
+      cmd = "dist/build/moore_skelboe/moore_skelboe";
       break;
     case roots:
-      cmd = "dist/build/rin/rin \"" + cmd + "\"";
+      cmd = "dist/build/rin/rin";
       break;
   }
-  dirty = false;
   xc = -1;
   yc = -1;
-  load();
+  rin = load(cmd, polynomial, xmin, xmax, ymin, ymax);
   init(argc, argv);
   return 0;
 }
 
 void motionFunc(int x, int y) {
+  mouse_rect->x1 = 2 * (x - (double)width/2)/width;
+  mouse_rect->y1 = 2 * (-y + (double)height/2)/height;
 
+  glutPostRedisplay();
 }
 
 void mouseFunc(int button, int state, int x, int y) {
@@ -174,18 +170,37 @@ void mouseFunc(int button, int state, int x, int y) {
     if (state == GLUT_DOWN) {
       xc = x;
       yc = y;
+      double world_x = 2 * (x - (double)width/2)/width;
+      double world_y = 2 * (-y + (double)height/2)/height;
+      mouse_rect = new Box(world_x, world_x, world_y, world_y);
     } else {
+      free(mouse_rect);
+      mouse_rect = NULL;
       if (xc != x && yc != y) {
-        double width = XSCALE * 2 / glutGet(GLUT_WINDOW_WIDTH);
-        double height = YSCALE * 2 / glutGet(GLUT_WINDOW_HEIGHT);
+        //double width = XSCALE / glutGet(GLUT_WINDOW_WIDTH);
+        //double height = YSCALE / glutGet(GLUT_WINDOW_HEIGHT);
 
-        dirty = true;
-        xmin += std::min(xc, x) * width;
-        xmax = xmin + std::max(xc, x) * width;
-        ymin += std::min(yc, y) * height;
-        ymax = ymin + std::max(yc, y) * height;
+        double screen_xmin = std::min(xc, x) / (double) width;
+        double screen_ymin = std::min(height - yc, height - y) / (double) height;
+
+        double side_len = std::max(std::abs(xc - x), std::abs(yc - y));
+
+        double screen_xmax = screen_xmin + side_len / (double) width;
+        double screen_ymax = screen_ymin + side_len / (double) height;
+
+        xmin = xmin + screen_xmin * XSCALE;
+        xmax = xmin + screen_xmax * XSCALE;
+        ymin = ymin + screen_ymin * YSCALE;
+        ymax = ymin + screen_ymax * YSCALE;
+
         pclose(rin);
-        load();
+        rin = load(cmd, polynomial, xmin, xmax, ymin, ymax);
+
+        potential_solutions.clear();
+        discarded_boxes.clear();
+
+        if (display_mode == minimization)
+          current_min = std::numeric_limits<double>::max();
       }
 
       xc = -1;
@@ -207,27 +222,98 @@ void prune_min_solution_boxes(double new_min) {
   }
 }
 
-void draw_box(Box box) {
+// Draws a box. If emphasize is true, it is colored white. Otherwise, it is
+// colored its own color
+void draw_box(Box box, bool emphasize) {
   glBegin(GL_POLYGON);
-    glColor3d(box.r, box.g, box.b);
-    glVertex2d(box.x0, box.y0);
-    glVertex2d(box.x1, box.y0);
-    glVertex2d(box.x1, box.y1);
-    glVertex2d(box.x0, box.y1);
-  glEnd();
-}
-
-void draw_outline(Box box, bool emphasize) {
-  glBegin(GL_LINE_LOOP);
     if (emphasize) {
       glColor3d(1, 1, 1);
     } else {
-      glColor3d(0, 0, 0);
+      glColor3d(box.r, box.g, box.b);
     }
-
     glVertex2d(box.x0, box.y0);
     glVertex2d(box.x1, box.y0);
     glVertex2d(box.x1, box.y1);
     glVertex2d(box.x0, box.y1);
   glEnd();
 }
+
+// Draws the outline of a box. If emphasize is true, it is colored white.
+// Otherwise, it is colored its own color
+void draw_outline(Box box, bool emphasize) {
+  if (emphasize) {
+    glColor3d(1, 1, 1);
+  } else {
+    glColor3d(0.5 * box.r, 0.5 * box.g, 0.5 * box.b);
+  }
+
+  glBegin(GL_LINE_LOOP);
+    glVertex2d(box.x0, box.y0);
+    glVertex2d(box.x1, box.y0);
+    glVertex2d(box.x1, box.y1);
+    glVertex2d(box.x0, box.y1);
+  glEnd();
+}
+
+Box::Box() {
+  next = NULL;
+}
+Box::Box(Json::Value val, Mode display_mode) {
+  x0 = to_x_world_coord(val["input"][0]["low"].asDouble());
+  x1 = to_x_world_coord(val["input"][0]["high"].asDouble());
+  y0 = to_y_world_coord(val["input"][1]["low"].asDouble());
+  y1 = to_y_world_coord(val["input"][1]["high"].asDouble());
+
+  low = val["output"]["low"].asDouble();
+  high = val["output"]["high"].asDouble();
+  next = NULL;
+  switch (display_mode) {
+    case roots:
+      valid = high > 0 && low < 0;
+      break;
+    case minimization:
+      valid = low < current_min;
+      prune_min_solution_boxes(current_min);
+      break;
+  }
+
+  double max_noise = 0.1;
+  double r_noise = max_noise * ((double)rand()/(double)RAND_MAX) - max_noise/2;
+  double g_noise = max_noise * ((double)rand()/(double)RAND_MAX) - max_noise/2;
+  double b_noise = max_noise * ((double)rand()/(double)RAND_MAX) - max_noise/2;
+
+  // Set box color based on function value
+  switch (display_mode) {
+    case roots:
+      low = (high + low) / 2;
+      high = std::min(log(abs(low) + 1), 1.0) * 0.6 + 0.4;
+      r = low < 0 ? high : 0.2 + r_noise;
+      g = 0.2 + g_noise;
+      b = low > 0 ? high : 0.2 + b_noise;
+      break;
+    case minimization:
+      r = 0.4 + r_noise;
+      g = 0.2 + g_noise;
+      b = 0.8 + b_noise;
+      break;
+  }
+}
+
+void reshapeFunc(GLint gl_width, GLint gl_height) {
+  width  = gl_width;
+  height = gl_height;
+  glutPostRedisplay();
+}
+
+// Take an x coordinate from the domain we are working on and convert
+// it into world space to be drawn
+double to_x_world_coord(double math_coord) {
+  return 2 * (math_coord - xmin) / (double) XSCALE - 1;
+}
+
+// Take a y coordinate from the domain we are working on and convert
+// it into world space to be drawn
+double to_y_world_coord(double math_coord) {
+  return 2 * (math_coord - ymin) / (double) YSCALE - 1;
+}
+
